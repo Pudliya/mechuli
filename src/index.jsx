4dab2a72408caab7481c4d0e644884dfeae68b39
volant97 <volant97@naver.com>
2023-12-05 20:30:19 +0900

    [SCRIPT] 초기 세팅 추가

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Reset from './style/Reset';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <>
+    <App />
+    <Reset />
+  </>
+);
