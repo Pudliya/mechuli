a46aed54f170f38665259b000519561251923881
kwanghee <rkdgh4427@gmail.com>
2023-12-05 21:33:12 +0900

    [ADD] Layout 검색 창 틀 완성 기능 필요

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Layout from './components/Layout';
 import Home from './pages/Home';
 import Reset from './style/Reset';
 
 function App() {
   return (
     <>
+      <Layout />
       <Home />
       <Reset />
     </>
---
+++ src/components/Layout.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FiSearch } from 'react-icons/fi';
+import mechuliLogo from '../assets/logo/mechuli_logo.png';
+import {
+  IconBtn,
+  LayoutDiv,
+  Logo,
+  SearchForm,
+  SearchInput,
+  SearchInputContainer
+} from '../style/LayoutStyled';
+
+function Layout() {
+  return (
+    <>
+      <LayoutDiv>
+        <Logo>
+          <img
+            src={mechuliLogo}
+            alt="Mechuli 로고"
+            style={{ height: '50px' }}
+          />
+        </Logo>
+        <SearchForm>
+          <SearchInputContainer>
+            <SearchInput placeholder="검색어를 입력해주세요." />
+            <IconBtn>
+              <FiSearch />
+            </IconBtn>
+          </SearchInputContainer>
+        </SearchForm>
+      </LayoutDiv>
+    </>
+  );
+}
+
+export default Layout;
