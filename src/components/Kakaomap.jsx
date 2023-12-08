65fd12f32a74b1da01f7124badadd5de8aba559f
volant97 <volant97@naver.com>
2023-12-08 20:31:18 +0900

    [UPDATE] merge

--- src/components/Kakaomap.jsx
+++ src/components/Kakaomap.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef, useState } from 'react';
 import { StMap } from '../style/KakaomapStyled';
 import { useDispatch, useSelector } from 'react-redux';
 import { setPlace } from '../redux/slices/placeSlice';
+import defaultMarker from '../assets/marker/defaultMarker.png';
+import styled from 'styled-components';
 
 function Kakaomap({
   searchPlace,
@@ -97,24 +99,12 @@ function Kakaomap({
     const contentNode = document.createElement('div');
     contentNodeRef.current = contentNode;
     contentNode.className = 'placeinfo_wrap';
-    addEventHandle(contentNode, 'mousedown', kakao.maps.event.preventMap);
-    addEventHandle(contentNode, 'touchstart', kakao.maps.event.preventMap);
-
-    infowindow.current.setContent(contentNode);
 
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, [searchBtnToggle]);
 
   // ------useEffect-------
-
-  function addEventHandle(target, type, callback) {
-    if (target.addEventListener) {
-      target.addEventListener(type, callback);
-    } else {
-      target.attachEvent('on' + type, callback);
-    }
-  }
 
   const handleResize = () => {
     const map = mapRef.current;
@@ -159,7 +149,6 @@ function Kakaomap({
       for (let i = 0; i < data.length; i++) {
         displayMarker(data[i]);
         bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
-        // =======================
         newPlaces.push({
           id: data[i].id,
           category_name: data[i].category_name,
@@ -184,25 +173,67 @@ function Kakaomap({
   };
 
   const displayMarker = (place) => {
+    console.log('place : ', place);
     let marker = new kakao.maps.Marker({
       map: mapRef.current,
       position: new kakao.maps.LatLng(place.y, place.x)
     });
 
+    let content =
+      '<div class="placeinfo">' +
+      '   <a class="title" href="' +
+      place.place_url +
+      '" target="_blank" title="' +
+      place.place_name +
+      '">' +
+      place.place_name +
+      '</a>';
+
+    if (place.road_address_name) {
+      content +=
+        '    <span title="' +
+        place.road_address_name +
+        '">' +
+        place.road_address_name +
+        '</span>' +
+        '  <span class="jibun" title="' +
+        place.address_name +
+        '">(지번 : ' +
+        place.address_name +
+        ')</span>';
+    } else {
+      content +=
+        '    <span title="' +
+        place.address_name +
+        '">' +
+        place.address_name +
+        '</span>';
+    }
+    content +=
+      '    <span class="tel">' +
+      place.phone +
+      '</span>' +
+      '</div>' +
+      '<div class="after"></div>';
+
+    const customOverlay = new kakao.maps.CustomOverlay({
+      map: mapRef.current,
+      clickable: true,
+      content,
+      position: new kakao.maps.LatLng(place.y, place.x),
+      xAnchor: 0.5,
+      yAnchor: 1,
+      zIndex: 3
+    });
+
+    customOverlay.setMap(null);
+
     kakao.maps.event.addListener(marker, 'click', function () {
-      infowindow.current.setContent(
-        '<div style="font-size:12px; padding:5px">' +
-          place.place_name +
-          '<br/>' +
-          place.address_name +
-          '</div>'
-      );
+      customOverlay.setMap(mapRef.current);
 
       kakao.maps.event.addListener(mapRef.current, 'click', function () {
-        infowindow.current.close();
+        customOverlay.setMap(null);
       });
-
-      infowindow.current.open(mapRef.current, marker);
     });
   };
 
@@ -216,10 +247,165 @@ function Kakaomap({
   }, [latlng]);
 
   return (
-    <>
+    <StContainer>
       <StMap id="myMap"></StMap>
-    </>
+    </StContainer>
   );
 }
 
 export default Kakaomap;
+
+const StContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  border: 2px solid blue;
+
+  #myMap {
+    width: 100%;
+    height: 100vh;
+    position: absolute;
+    overflow: hidden;
+  }
+
+  &,
+  & * {
+    margin: 0;
+    padding: 0;
+    font-family: 'Malgun Gothic', dotum, '돋움', sans-serif;
+    font-size: 12px;
+  }
+  & {
+    position: relative;
+    width: 100%;
+    height: 350px;
+  }
+  #category {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    border-radius: 5px;
+    border: 1px solid #909090;
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.4);
+    background: #fff;
+    overflow: hidden;
+    z-index: 2;
+  }
+  #category li {
+    float: left;
+    list-style: none;
+    width: 50px;
+    border-right: 1px solid #acacac;
+    padding: 6px 0;
+    text-align: center;
+    cursor: pointer;
+  }
+  #category li.on {
+    background: #eee;
+  }
+  #category li:hover {
+    background: #ffe6e6;
+    border-left: 1px solid #acacac;
+    margin-left: -1px;
+  }
+  #category li:last-child {
+    margin-right: 0;
+    border-right: 0;
+  }
+  #category li span {
+    display: block;
+    margin: 0 auto 3px;
+    width: 27px;
+    height: 28px;
+  }
+  #category li .category_bg {
+    background: url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/places_category.png')
+      no-repeat;
+  }
+  #category li .bank {
+    background-position: -10px 0;
+  }
+  #category li .mart {
+    background-position: -10px -36px;
+  }
+  #category li .pharmacy {
+    background-position: -10px -72px;
+  }
+  #category li .oil {
+    background-position: -10px -108px;
+  }
+  #category li .cafe {
+    background-position: -10px -144px;
+  }
+  #category li .store {
+    background-position: -10px -180px;
+  }
+  #category li.on .category_bg {
+    background-position-x: -46px;
+  }
+  .placeinfo_wrap {
+    position: absolute;
+    bottom: 28px;
+    left: -150px;
+    width: 300px;
+  }
+  .placeinfo {
+    position: relative;
+    width: 100%;
+    border-radius: 6px;
+    border: 1px solid #ccc;
+    border-bottom: 2px solid #ddd;
+    padding-bottom: 10px;
+    background: #fff;
+  }
+  .placeinfo:nth-of-type(n) {
+    border: 0;
+    box-shadow: 0px 1px 2px #888;
+  }
+  .placeinfo_wrap .after {
+    content: '';
+    position: relative;
+    margin-left: -12px;
+    left: 50%;
+    width: 22px;
+    height: 12px;
+    background: url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white.png');
+  }
+  .placeinfo a,
+  .placeinfo a:hover,
+  .placeinfo a:active {
+    color: #fff;
+    text-decoration: none;
+  }
+  .placeinfo a,
+  .placeinfo span {
+    display: block;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+  .placeinfo span {
+    margin: 5px 5px 0 5px;
+    cursor: default;
+    font-size: 13px;
+  }
+  .placeinfo .title {
+    font-weight: bold;
+    font-size: 14px;
+    border-radius: 6px 6px 0 0;
+    margin: -1px -1px 0 -1px;
+    padding: 10px;
+    color: #fff;
+    background: #d95050;
+    background: #d95050
+      url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/arrow_white.png')
+      no-repeat right 14px center;
+  }
+  .placeinfo .tel {
+    color: #0f7833;
+  }
+  .placeinfo .jibun {
+    color: #999;
+    font-size: 11px;
+    margin-top: 0;
+  }
+`;
