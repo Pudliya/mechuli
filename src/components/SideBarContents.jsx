ccd836284cd5d0d9b7e7edc6c17ba13177936cd6
yachaechae <wpql1207@gmail.com>
2023-12-08 19:53:16 +0900

    [ADD] 음식 svg 아이콘 react 로 컴포넌트화

--- src/components/SideBarContents.jsx
+++ src/components/SideBarContents.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { StSideBar } from '../style/StSideBar';
 import SideBarCategory from './SideBarCategory';
 import SideBarLIst from './SideBarLIst';
+import ChineseFood from '../assets/logo/icon/ChineseFood';
+import KoreanFood from '../assets/logo/icon/KoreanFood';
+import DefaultIcon from '../assets/logo/icon/DefaultIcon';
+import JapaneseFood from '../assets/logo/icon/JapaneseFood';
+import WesternFood from '../assets/logo/icon/WesternFood';
 
 export default function SideBarContents() {
   return (
@@ -10,6 +15,12 @@ export default function SideBarContents() {
         <SideBarCategory />
         <h2>맛집 리스트</h2>
         <SideBarLIst />
+
+        <ChineseFood />
+        <KoreanFood />
+        <DefaultIcon />
+        <WesternFood />
+        <JapaneseFood />
       </StSideBar>
     </>
   );
