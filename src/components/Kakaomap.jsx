76573d26347573a0936e240159d169a0e31403a6
yachaechae <wpql1207@gmail.com>
2023-12-09 02:10:24 +0900

    기존 html 태그 ` 태그를 이용하여 수정

--- src/components/Kakaomap.jsx
+++ src/components/Kakaomap.jsx
@@ -182,44 +182,17 @@ function Kakaomap({
       position: new kakao.maps.LatLng(place.y, place.x)
     });
 
-    //  let content = ` <div  class="placeinfo">   <a class="title" href="${place.place_url}" target="_blank" title="${place.place_name}">${place.place_name}</a>`;
-
-    let content =
-      '<div class="placeinfo">' +
-      '   <a class="title" href="' +
-      place.place_url +
-      '" target="_blank" title="' +
-      place.place_name +
-      '">' +
-      place.place_name +
-      '</a>';
+    let content = ` <div  class="placeinfo">   <a class="title" href="${place.place_url}" target="_blank" title="${place.place_name}">${place.place_name}</a>`;
 
     if (place.road_address_name) {
-      content +=
-        '    <span title="' +
-        place.road_address_name +
-        '">' +
-        place.road_address_name +
-        '</span>' +
-        '  <span class="jibun" title="' +
-        place.address_name +
-        '">(지번 : ' +
-        place.address_name +
-        ')</span>';
+      content += `<span title="${place.road_address_name}"></span>
+         <span class="jibun" title=" ${place.address_name}">(지번 : ${place.address_name} )</span>`;
     } else {
-      content +=
-        '    <span title="' +
-        place.address_name +
-        '">' +
-        place.address_name +
-        '</span>';
+      content += `<span title=" ${place.address_name} ">
+        ${place.address_name} </span>`;
     }
-    content +=
-      '    <span class="tel">' +
-      place.phone +
-      '</span>' +
-      '</div>' +
-      '<div class="after"></div>';
+    content += `  <span class="tel">
+      ${place.phone} </span></div><div class="after"></div>`;
 
     const customOverlay = new kakao.maps.CustomOverlay({
       map: mapRef.current,
