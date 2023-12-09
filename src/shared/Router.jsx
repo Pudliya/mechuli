103eecf6a5355c366842699f2e48b6ddd76e3ea2
Jihunee <jihun6439@daum.net>
2023-12-10 00:13:48 +0900

    [FIX] layOut을 Home으로 수정완료

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Layout from '../components/Layout';
+import Home from '../pages/Home';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
